9cad42ee45b71e7567088d921e3d03baec099dd2
Srihari N <sriharin207@gmail.com>
2023-05-04 23:34:15 +0530

    updated server.js

--- server.js
+++ server.js
@@ -25,4 +25,4 @@ app.get("/", (req, res) => res.send("hello world"));
 
 app.use(customErrorHandler);
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
