41e0038690bee3c55cb2a0f6234b90f1d4b1c1f8
Srihari N <sriharin207@gmail.com>
2023-05-04 23:21:46 +0530

    updated routers.js

--- routes/router.js
+++ routes/router.js
@@ -15,5 +15,5 @@ router.post("/submitOrder", control.submitOrder);
 // router.get("/getOrderedItemsData", verifyToken, control.getOrderedItemsData);
 router.get("/getOrderedItemsData", control.getOrderedItemsData);
 
-router.all("*", control.invalidReq);
+// router.all("*", control.invalidReq);
 module.exports = router;
